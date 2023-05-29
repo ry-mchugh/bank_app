59188b109aa14e2bf46099bcd0f0faabf7dce62b
ry-mchugh <ry.mchugh@outlook.com>
2023-05-29 23:22:13 +0100

    rename variable

--- StarlingBank.java
+++ StarlingBank.java
@@ -3,6 +3,6 @@ public class StarlingBank {
     public static void main(String[] args) {
 
         Account ryan = new Account("Ryan McHugh", "A00001");
-        ryan.showMenu();
+        ryan.menu();
     }
 }
