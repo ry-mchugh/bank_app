3a7aafab9ea27f5edd71445e48e2db4b16bd6b8c
ry-mchugh <ry.mchugh@outlook.com>
2023-05-29 23:13:46 +0100

    add function to create option menu

--- Account.java
+++ Account.java
@@ -47,5 +47,45 @@ public class Account {
         System.out.println("After " + years + " years, your balance will be: " + newBalance);
     }
 
+    void menu() {
+        char option = '\0';
+        Scanner scanner = new Scanner(system.in);
+        System.out.println("Welcome, " + customerName + ".");
+        System.out.println("Your ID is: " + customerID);
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("A. Check balance");
+        System.out.println("B. Make a deposit");
+        System.out.println("C. Make a withdrawal");
+        System.out.println("D. View previous transaction");
+        System.out.println("E. Calculate interest");
+        System.out.println("F. Exit");
 
+        do {
+            System.out.println();
+            System.out.println("Enter an option: ");
+            char option1 = scanner.next().charAt(0);
+            option = Character.toUpperCase(option1);
+            System.out.println();
+
+            switch(option) {
+                case 'A':
+                    System.out.println("++++++++++++++++++++++");
+                    System.out.println("Balance = £" + balance);
+                    System.out.println("++++++++++++++++++++++");
+                    System.out.println();
+                    break;
+
+                case 'B':
+                    System.out.println("Enter an amount to deposit: ");
+                    int amount = scanner.nextInt();
+                    deposit(amount);
+                    System.out.println();
+                    break;
+
+
+            }
+        }
+    }
 }
