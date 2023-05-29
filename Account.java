a668a27037a35f2d4247b953945a64a8f6dac83f
ry-mchugh <ry.mchugh@outlook.com>
2023-05-29 22:52:54 +0100

    add class variables and constructor

--- Account.java
+++ Account.java
@@ -1,4 +1,16 @@
 import java.util.Scanner;
 public class Account {
+    // class variables
+    int balance;
+    int prevTransaction;
+    String customerName;
+    String customerID;
+
+    // class constructor
+    Account(String cname, String cid) {
+        customerName = cname;
+        customerID = cid;
+    }
+
 
 }
